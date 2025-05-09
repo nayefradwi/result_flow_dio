cf59b75a627742d67c408d9c7a956c5fb7dbf72e
naif radwi <naif.mazen.radwi@gmail.com>
2025-05-09 17:15:57 +0300

    init

---
+++ example/result_flow_dio_example.dart
@@ -0,0 +1,6 @@
+import 'package:result_flow_dio/result_flow_dio.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/result_flow_dio_test.dart
@@ -0,0 +1,16 @@
+import 'package:result_flow_dio/result_flow_dio.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
